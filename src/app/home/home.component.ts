894143a280409bf58352aff85f1abb46809fca41
LucasBaumer <63851051+lucasbaumer@users.noreply.github.com>
2024-04-01 20:27:11 -0300

    feat: :clown_face: add home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  tenis01 = "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/fabaa569-6800-419f-be28-cf794a467636/air-max-correlate-womens-shoes-29Vjp0.png"
+  tenis02 = "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/752efdf6-b2b7-47fc-b137-5a70b5dd9423/air-force-1-07-mens-shoes-5xqQ6q.png"
+  tenis03 = "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/db2a59f9-944f-4e07-838f-dd90ad700e7a/dunk-low-retro-mens-shoes-WhCkmQ.png"
 }
+
